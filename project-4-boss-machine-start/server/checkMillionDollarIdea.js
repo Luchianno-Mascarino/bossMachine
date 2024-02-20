e6746478aabdee8df6ffe4e609bb7757160261d8
Luchianno Mascarino <125408126+Luchianno-Mascarino@users.noreply.github.com>
2024-02-20 16:41:35 -0300

    ADD OneMillionDollarIdea middleware

--- project-4-boss-machine-start/server/checkMillionDollarIdea.js
+++ project-4-boss-machine-start/server/checkMillionDollarIdea.js
@@ -1,4 +1,13 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const { numWeeks, weeklyRevenue } = req.body
+    const ideaValue = numWeeks * weeklyRevenue;
+
+    if(ideaValue < 1000000){
+        return res.status(404).json({error: 'It is not a one million idea'})
+    }
+
+    next();
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
